9b700853dd9eecd3eee50e031cc5d6720928f3ba
Bluemo <shutaro.aoyama@gmail.com>
2020-03-27 01:10:44 +0900

    Implemented time-controlling experience

--- KinetoPrototype/ViewController.swift
+++ KinetoPrototype/ViewController.swift
@@ -11,53 +11,70 @@ import AVFoundation
 
 class ViewController: UIViewController {
 
-    // デバイスからの入力と出力を管理するオブジェクトの作成
     var captureSession = AVCaptureSession()
-    // カメラデバイスそのものを管理するオブジェクトの作成
-    // メインカメラの管理オブジェクトの作成
     var mainCamera: AVCaptureDevice?
-    // インカメの管理オブジェクトの作成
     var innerCamera: AVCaptureDevice?
-    // 現在使用しているカメラデバイスの管理オブジェクトの作成
     var currentDevice: AVCaptureDevice?
-    // キャプチャーの出力データを受け付けるオブジェクト
     var videoOutput : AVCaptureVideoDataOutput?
-    // プレビュー表示用のレイヤ
     var cameraPreviewLayer : AVCaptureVideoPreviewLayer?
+    
+    var timer = Timer()
 
+    @IBOutlet weak var fpsSlider: UISlider!
     @IBOutlet weak var previewImageView: UIImageView!
+    @IBOutlet weak var currentFpsLabel: UILabel!
+    
+    var pastImages: [UIImage] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupCaptureSession()
         setupDevice()
         setupInputOutput()
-        //setupPreviewLayer()
         captureSession.startRunning()
         
-        print(captureSession.connections.first?.videoOrientation = .portraitUpsideDown)
+        runTimer(fps: 30.0)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func fpsValueChanged(_ sender: UISlider) {
+        runTimer(fps: sender.value)
+    }
+    
+    func runTimer(fps: Float) {
+        currentFpsLabel.text = String(format: "x%.3f", fps/30)
+        timer.invalidate()
+        timer = Timer.scheduledTimer(withTimeInterval: TimeInterval((1/fps)), repeats: true, block: { (timer) in
+            if self.pastImages.count > 0 {
+                do {
+                    try self.previewImageView.image = self.pastImages.first //FIX: EXC_BAD_ACCESS起こる
+                    self.pastImages.remove(at: 0)
+                } catch {
+                    print("frame update error")
+                }
+            }
+        })
+    }
+    
+    @IBAction func backToTheFuture(_ sender: Any) {
+        pastImages = []
+        runTimer(fps: 30)
+    }
 }
 
-//MARK: カメラ設定メソッド
+
 extension ViewController{
-    // カメラの画質の設定
     func setupCaptureSession() {
-        captureSession.sessionPreset = AVCaptureSession.Preset.high
+        captureSession.sessionPreset = AVCaptureSession.Preset.medium
     }
 
-    // デバイスの設定
     func setupDevice() {
-        // カメラデバイスのプロパティ設定
         let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [AVCaptureDevice.DeviceType.builtInWideAngleCamera], mediaType: AVMediaType.video, position: AVCaptureDevice.Position.unspecified)
-        // プロパティの条件を満たしたカメラデバイスの取得
         let devices = deviceDiscoverySession.devices
 
         for device in devices {
@@ -67,52 +84,40 @@ extension ViewController{
                 innerCamera = device
             }
         }
-        // 起動時のカメラを設定
         currentDevice = mainCamera
     }
 
-    // 入出力データの設定
+
     func setupInputOutput() {
         do {
-            // 指定したデバイスを使用するために入力を初期化
+
             let captureDeviceInput = try AVCaptureDeviceInput(device: currentDevice!)
-            // 指定した入力をセッションに追加
             captureSession.addInput(captureDeviceInput)
-            
-            // 出力データを受け取るオブジェクトの作成
+
             videoOutput = AVCaptureVideoDataOutput()
-            // 出力設定: カラーチャンネル
             videoOutput!.videoSettings = [kCVPixelBufferPixelFormatTypeKey : kCVPixelFormatType_32BGRA] as [String : Any]
-             // 出力設定: デリゲート、画像をキャプチャするキュー
             videoOutput!.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
-             // 出力設定: キューがブロックされているときに新しいフレームが来たら削除
             videoOutput!.alwaysDiscardsLateVideoFrames = true
             
             captureSession.addOutput(videoOutput!)
+            
+            if currentDevice == innerCamera {
+                captureSession.connections.first?.videoOrientation = .portrait
+            } else {
+                captureSession.connections.first?.videoOrientation = .portraitUpsideDown
+            }
+        
         } catch {
             print(error)
         }
     }
-
-    // カメラのプレビューを表示するレイヤの設定 NOT USED
-    func setupPreviewLayer() {
-        // 指定したAVCaptureSessionでプレビューレイヤを初期化
-        self.cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
-        // プレビューレイヤが、カメラのキャプチャーを縦横比を維持した状態で、表示するように設定
-        self.cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
-        // プレビューレイヤの表示の向きを設定
-        self.cameraPreviewLayer?.connection?.videoOrientation = AVCaptureVideoOrientation.landscapeRight
-
-        self.cameraPreviewLayer?.frame = view.frame
-        self.view.layer.insertSublayer(self.cameraPreviewLayer!, at: 0)
-    }
 }
 
 extension ViewController: AVCaptureVideoDataOutputSampleBufferDelegate {
-    // delegateメソッド
+
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         let image = imageFromSampleBuffer(sampleBuffer: sampleBuffer)
-        DispatchQueue.main.async { self.previewImageView.image = image }
+        self.pastImages.append(image)
     }
     
     func imageFromSampleBuffer(sampleBuffer :CMSampleBuffer) -> UIImage {
